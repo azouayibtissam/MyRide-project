997f9887cb925ca088b45f8a24a02ce29c6bfa0d
azouayibtissam <ibtissam13azouay@gmail.com>
2023-01-26 23:40:39 +0100

    add forms to html pages

--- MyRide/startup/urls.py
+++ MyRide/startup/urls.py
@@ -1,6 +1,9 @@
 from django import views
 from django.urls import path, include
-from . import views
+from .views import *
 urlpatterns = [
-    path('',views.home_view),
+    path('',home, name="home"),
+    path('login',login, name="login"),
+    path('signup',signup, name="signup"),
+    path('mdp',mdp, name="mdp"),
 ]
--- MyRide/startup/views.py
+++ MyRide/startup/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
-def home_view(request):
+def home(request):
     return render(request, 'home.html')
+
+def login(request):
+    return render(request, 'login.html')
+
+def signup(request):
+    return render(request, 'signup.html')
+
+def mdp(request):
+    return render(request, 'mdp.html')
